6e1304e608cc33c63dc3b1a3cb811fe458459257
meruem243 <mughees.ahmed243@gmail.com>
2023-07-23 00:32:08 +0500

    Using the Expanded Widget to Create Flexible Layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
           backgroundColor: Colors.red,
+          title: const Center(
+            child: Text(
+              "Dice",
+              style: TextStyle(
+                fontSize: 50.0,
+                color: Colors.blue,
+              ),
+            ),
+          ),
         ),
-        body: DicePage(),
+        backgroundColor: Colors.red[800],
+        body: const DicePage(),
       ),
     ),
   );
 }
 
 class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        const Expanded(
+          // flex: 1,
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          // flex: 2,
+          child: Image.asset('images/dice1.png'),
+        ),
+      ],
+    );
   }
 }
